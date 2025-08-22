bcf446956359051dd9c651221c2fb22c9248cb50
Simone Desantis <s.desantis@altravia.com>
2025-08-22 09:04:21 +0000

    aggiunti exchange automotive e inizio sistemazione

--- memory_store.py
+++ memory_store.py
@@ -6,6 +6,6 @@ callback_results = {}
 # #prelevo le variabili ambiente 
 K_SERVICE = os.getenv("K_SERVICE")
 DEV_VM = os.getenv("X-DEV-VM")
-localDomain = K_SERVICE.replace("-",".")
-localDomain = localDomain.replace("alpha","dev") if DEV_VM and localDomain == "alpha.mcp.openapi.com" else localDomain
+callbackUrl = "https://" + K_SERVICE.replace("-", ".") + "/callbacks";
+callbackUrl = callbackUrl.replace("alpha","dev") if DEV_VM else callbackUrl
 
