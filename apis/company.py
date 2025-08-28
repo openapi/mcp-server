3f647df4e39697afe8af0e8b68d00ac501fb8619
Simone Desantis <s.desantis@altravia.com>
2025-08-28 11:05:45 +0000

    manage string submissions

--- apis/company.py
+++ apis/company.py
@@ -114,45 +114,43 @@ async def get_company_IT_search(
     ctx: Context,
     companyName: Union[str, None] = None,
     province: Union[str, None] = None,
-    skip: Union[int, None] = None,
-    limit: int = 10,
+    skip: Union[int, None, str] = None,
+    limit: Union[int, None, str] = None,
     dataEnrichment: str = "name",
-    sortBy: Union[str, None] = None,
-    vatGroup: Union[bool, None] = None,
     startDate: Union[str, None] = None,
     endDate: Union[str, None] = None,
-    dryRun: Union[int, None] = None,
-    lat: Union[float, None] = None,
-    long: Union[float, None] = None,
-    radius: Union[int, None] = None,
+    dryRun: Union[int, None, str] = None,
+    lat: Union[float, None, str] = None,
+    long: Union[float, None, str] = None,
+    radius: Union[int, None, str] = None,
     autocomplete: Union[str, None] = None,
     townCode: Union[str, None] = None,
     atecoCode: Union[str, None] = None,
     cciaa: Union[str, None] = None,
     reaCode: Union[str, None] = None,
-    minTurnover: Union[int, None] = None,
-    maxTurnover: Union[int, None] = None,
-    minEmployees: Union[int, None] = None,
-    maxEmployees: Union[int, None] = None,
+    minTurnover: Union[int, None, str] = None,
+    maxTurnover: Union[int, None, str] = None,
+    minEmployees: Union[int, None, str] = None,
+    maxEmployees: Union[int, None, str] = None,
     sdiCode: Union[str, None] = None,
     legalFormCode: Union[str, None] = None,
     shareHolderTaxCode: Union[str, None] = None,
     activityStatus: Union[str, None] = None,
     pec: Union[str, None] = None,
-    creationTimestamp: Union[int, None] = None,
-    lastUpdateTimestamp: Union[int, None] = None
+    creationTimestamp: Union[int, None, str] = None,
+    lastUpdateTimestamp: Union[int, None, str] = None
 ) -> Any:
     """Returns a list of italian companies based on the search criteria. Use this tool if you don't know the VAT number of a company.
     Args:
         companyName: The name or part of it of an Italian company (optional).
         province: The province where the company is located to restrict the results (optional).
         skip: The number of records to skip for pagination (optional).
-        limit: The maximum number of results to return (default is 10).
-        dataEnrichment: Avoid further queries receiving Additional data enrichment options for the search (default is name),  Available values : start, advanced, pec, address, shareholders, name
+        limit: The maximum number of results to return (default is 10, in dryRun default is null). 
+        dataEnrichment: Avoid further queries receiving Additional data enrichment options in the results (default is name),  Available values : start, advanced, pec, address, shareholders, name
         legalFormCode: Filter by legalformcode of the company (optional). For Available values use get_company_IT_legal_forms_list
         startDate: Filter by the start date of the company (optional).
         endDate: Filter by the end date of the company (optional).
-        dryRun: Simulates a request by returning only the number of records found and the price (optional).
+        dryRun: Simulates a request by returning only the number of records found and the price (optional) Available values :0,1.
         lat: Latitude for geographical search (optional).
         long: Longitude for geographical search (optional).
         radius: Radius in meters for geographical search (optional).
@@ -166,12 +164,11 @@ async def get_company_IT_search(
         minEmployees: Minimum number of employees (optional).
         maxEmployees: Maximum number of employees (optional).
         sdiCode: SDI code (optional).
-        legalFormCode: Legal form code (optional).
         shareHolderTaxCode: Tax code of a company member (optional).
         activityStatus: Status of the company in the Chamber of Commerce (optional), Available values : ATTIVA, CESSATA, REGISTRATA, INATTIVA, SOSPESA, IN_ISCRIZIONE.
         pec: PEC email address of the company (optional).
-        creationTimestamp: Filter by creation timestamp (optional).
-        lastUpdateTimestamp: Filter by last update timestamp (optional).
+        creationTimestamp: Filter by creation unix timestamp (optional).
+        lastUpdateTimestamp: Filter by last update unix timestamp (optional).
     """
     url = f"https://company.openapi.com/IT-search?limit={limit}"
 
@@ -180,16 +177,14 @@ async def get_company_IT_search(
             url += f"&companyName={companyName}"
     if province:
         url += f"&province={province}"
-    if skip is not None:
+    if skip is not None and skip is not "null":
         url += f"&skip={skip}"
+    if limit is not None and limit is not "null":
+        url += f"&limit={limit}"
+    if dryRun is None and limit is None:
+        url += f"&limit=10"
     if dataEnrichment:
         url += f"&dataEnrichment={dataEnrichment}"
-    if sortBy:
-        url += f"&sortBy={sortBy}"
-    if vatGroup is not None:
-        url += f"&vatGroup={str(vatGroup).lower()}"
-    if legalForm:
-        url += f"&legalForm={legalForm}"
     if startDate:
         url += f"&startDate={startDate}"
     if endDate:
@@ -258,9 +253,12 @@ async def get_company_WW_top(vat_or_taxCode: str,country_code: str, ctx: Context
 
 @mcp.tool
 async def get_company_IT_legal_forms_list(ctx: Context) -> Any:
-    """Obtain all the legal forms and codes available in italy usefull with search.
+    """Obtain all the updated legalFormsCodes and descriptions available in italy. Usefull with get_company_IT_search.
+
+    "EI"="ENTE IMPRESA","EL"="ENTE SOCIALE","OO"="COOPERATIVA SOCIALE","OS"="SOCIETA' CONSORTILE COOPERATIVA A RESPONSABILITA' LIMITATA","RC"="CONTRATTO DI RETE DOTATO DI SOGGETTIVITA' GIURIDICA","ST"="SOGGETTO ESTERO","AF"="ALTRE FORME","AP"="AZIENDA PROVINCIALE","AS"="SOCIETA' IN ACCOMANDITA SEMPLICE","CO"="CONSORZIO","EE"="ENTE ECCLESIASTICO","FO"="FONDAZIONE","SL"="SOCIETA' CONSORTILE A RESPONSABILITA' LIMITATA","SR"="SOCIETA' A RESPONSABILITA' LIMITATA","SZ"="SOCIETA' NON PREVISTA DALLA LEGISLAZIONE ITALIANA","AL"="AZIENDA SPECIALE","AN"="SOCIETA' CONSORTILE IN NOME COLLETTIVO","CC"="CONSORZIO CON ATTIVITA' ESTERNA","CF"="CONSORZIO FIDI","EC"="ENTE PUBBLICO COMMERCIALE","ED"="ENTE DIRITTO PUBBLICO","EN"="ENTE","OC"="SOCIETA' COOPERATIVA CONSORTILE","PA"="ASSOCIAZIONE IN PARTECIPAZIONE","SF"="SOCIETA' DI FATTO","SO"="SOCIETA' CONSORTILE PER AZIONI","AA"="SOCIETA' IN ACCOMANDITA PER AZIONI","AC"="ASSOCIAZIONE","AI"="ASSOCIAZIONE IMPRESA","AM"="AZIENDA MUNICIPALE","AT"="AZIENDA AUTONOMA STATALE","CR"="CONSORZIO INTERCOMUNALE","ES"="ENTE DI CUI ALLA L.R. 21-12-93 N88","GE"="GRUPPO EUROPEO DI INTERESSE ECONOMICO","IF"="IMPRESA FAMILIARE","LL"="AZIENDA SPECIALE DI CUI AL DLGS 267/2000","RS"="SOCIETA' A RESPONSABILITA' LIMITATA SEMPLIFICATA","SI"="SOCIETA' IRREGOLARE","AR"="AZIENDA REGIONALE","CS"="CONSORZIO SENZA ATTIVITA' ESTERNA","SA"="SOCIETA' ANONIMA","SC"="SOCIETA' COOPERATIVA","SD"="SOCIETA' EUROPEA","SG"="SOCIETA' COOPERATIVA EUROPEA","AE"="SOCIETA' CONSORTILE IN ACCOMANDITA SEMPLICE","EP"="ENTE PUBBLICO ECONOMICO","PF"="PERSONA FISICA","SN"="SOCIETA' IN NOME COLLETTIVO","SP"="SOCIETA' PER AZIONI","XX"="NON PRECISATA","AU"="SOCIETA'  PER AZIONI CON SOCIO UNICO","CE"="COMUNIONE EREDITARIA","CI"="SOCIETA' COOPERATIVA A RESPONSABILITA ILLIMITATA","CL"="SOCIETA' COOPERATIVA A RESPONSABILITA LIMITATA","CN"="SOCIETA' CONSORTILE","CZ"="CONSORZIO DI CUI AL DLGS 267/2000","DI"="IMPRESA INDIVIDUALE","RR"="SOCIETA' A RESPONSABILITA' LIMITATA A CAPITALE RIDOTTO","SE"="SOCIETA' SEMPLICE","SU"="SOCIETA' A RESPONSABILITA' LIMITATA CON UNICO SOCIO","AZ"="AZIENDA SPECIALE REA","CM"="CONSORZIO MUNICIPALE","EM"="ENTE MORALE","ER"="ENTE ECCLESIASTICO CIVILMENTE RICONOSCIUTO","FI"="FONDAZIONE IMPRESA","IC"="ISTITUTO DI CREDITO","ID"="ISTITUTO DI CREDITO DI DIRITTO PUBBLICO","IR"="ISTITUTO RELIGIOSO","MA"="MUTUA ASSICURAZIONE","PC"="PICCOLA SOCIETA' COOPERATIVA","PS"="PICCOLA SOCIETA' COOPERATIVA A RESPONSABILITA' LIMITATA","SM"="SOCIETA' DI MUTUO SOCCORSO","SS"="SOCIETA' COSTITUITA IN BASE A LEGGI DI ALTRO STATO","SV"="SOCIETA' TRA PROFESSIONISTI"
+
     """
-    print(f"Esecuzione tool: getTodayExchangeRates")
+    print(f"Esecuzione tool: get_company_IT_legal_forms_list")
     
     url = f"https://company.openapi.com/IT-legalforms/"
     api_call =  make_api_call(ctx, "GET", url)
