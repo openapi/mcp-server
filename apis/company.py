bd4cb89509f4a3a9a499a91d3975375505303e98
Simone Desantis <s.desantis@altravia.com>
2025-08-29 11:25:43 +0000

    altre flessibilità

--- apis/company.py
+++ apis/company.py
@@ -175,6 +175,7 @@ async def get_company_IT_search(
     """
     url = f"https://company.openapi.com/IT-search?limit={limit}"
 
+    
     if companyName:
         if companyName != "*" and companyName != "null":
             url += f"&companyName={companyName}"
