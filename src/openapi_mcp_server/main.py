85acafa71ddf47ebcddaf050f1bbc6c86463d658
Simone Desantis <s.desantis@openapi.com>
2025-09-10 11:04:49 +0200

    sistemate dipendenze

--- src/openapi_mcp_server/main.py
+++ src/openapi_mcp_server/main.py
@@ -4,7 +4,7 @@ import json
 from fastapi import FastAPI, Request, HTTPException, Response
 from src.openapi_mcp_server.memory_store import get_callback_result, set_callback_result  # usa sempre il singleton globale
 from src.openapi_mcp_server.mcp_core import mcp # Importa MCP e tool già registrati da mcp_core.py
-from apis import async_tool, company, cap, trust, visurecamerali, sms, risk, geocoding,automotive,exchange # Importa i tool (solo per triggerare la registrazione via @mcp.tool)
+from src.openapi_mcp_server.apis import async_tool, company, cap, trust, visurecamerali, sms, risk, geocoding,automotive,exchange # Importa i tool (solo per triggerare la registrazione via @mcp.tool)
 import asyncio
 from google.cloud import storage
 
