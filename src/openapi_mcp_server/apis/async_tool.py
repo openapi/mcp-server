ed51a56056b5c1ca2be16a404b53ef3992528b2e
Simone Desantis <s.desantis@altravia.com>
2025-09-10 08:42:35 +0000

    refactoring struttura directory non funzionante da testare in locale con venv

--- src/openapi_mcp_server/apis/async_tool.py
+++ src/openapi_mcp_server/apis/async_tool.py
@@ -1,8 +1,8 @@
 print("async_tool.py importato")
 from fastmcp import Context
 from typing import Any
-from mcp_core import make_api_call, mcp
-from memory_store import get_callback_result
+from src.openapi_mcp_server.mcp_core import mcp
+from src.openapi_mcp_server.memory_store import get_callback_result
 
 @mcp.tool
 async def check_async_status(request_id: str, ctx: Context) -> Any:
--- src/openapi_mcp_server/apis/trust.py
+++ src/openapi_mcp_server/apis/trust.py
@@ -1,9 +1,9 @@
 print("trust.py importato")
-from memory_store import callback_results, callbackUrl,SANDBOX_PREFIX
+from src.openapi_mcp_server.memory_store import callback_results, callbackUrl,SANDBOX_PREFIX
 from fastmcp import Context
 from typing import Any
 import asyncio
-from mcp_core import make_api_call, mcp, processPolling
+from src.openapi_mcp_server.mcp_core import make_api_call, mcp, processPolling
 import json
 
 @mcp.tool(
